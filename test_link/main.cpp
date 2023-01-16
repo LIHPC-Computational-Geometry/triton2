4ff2b721abe54efe65f90e21a4bd8af630ed67d6
nicolas le goff <nicolas.le-goff@cea.fr>
2023-01-16 22:58:17 +0100

    changed link_test implementation

--- test_link/main.cpp
+++ test_link/main.cpp
@@ -4,16 +4,48 @@
 /*----------------------------------------------------------------------------*/
 using namespace gmds;
 int main() {
-        MeshModel mod = DIM2|N|F|F2N;
-        IGMesh boundary_mesh(mod);
-        boundary_mesh.newNode(0,0);
-        boundary_mesh.newNode(0,1);
-        boundary_mesh.newNode(1,0);
-        boundary_mesh.newNode(1,1);
+        MeshModel mod = DIM3|N|F|F2N;
+        IGMesh plc(mod);
 
-        IGMesh vol_mesh(mod);
+        Node n0 = plc.newNode(0,0,0);
+        Node n1 = plc.newNode(0,10,0);
+        Node n2 = plc.newNode(10,10,0);
+        Node n3 = plc.newNode(10,0,0);
+
+        Node n4 = plc.newNode(0,0,10);
+        Node n5 = plc.newNode(0,10,10);
+        Node n6 = plc.newNode(10,10,10);
+        Node n7 = plc.newNode(10,0,10);
+
+        IGMesh::surface& surf1 = plc.newSurface("s1");
+        surf1.add(plc.newTriangle(n0,n1,n2));
+        surf1.add(plc.newTriangle(n0,n2,n3));
+
+        IGMesh::surface& surf2 = plc.newSurface("s2");
+        surf2.add(plc.newTriangle(n4,n6,n5));
+        surf2.add(plc.newTriangle(n4,n7,n6));
+
+        IGMesh::surface& surf3 = plc.newSurface("s3");
+        surf3.add(plc.newTriangle(n0,n1,n5));
+        surf3.add(plc.newTriangle(n0,n5,n4));
+
+        IGMesh::surface& surf4 = plc.newSurface("s4");
+        surf4.add(plc.newTriangle(n1,n2,n6));
+        surf4.add(plc.newTriangle(n1,n6,n5));
+
+        IGMesh::surface& surf5 = plc.newSurface("s5");
+        surf5.add(plc.newTriangle(n2,n3,n7));
+        surf5.add(plc.newTriangle(n2,n7,n6));
+
+        IGMesh::surface& surf6 = plc.newSurface("s6");
+        surf6.add(plc.newTriangle(n3,n0,n4));
+        surf6.add(plc.newTriangle(n3,n4,n7));
+
+
+        IGMesh vol_mesh(DIM3|R|F|F2N|N|R2N);
+
 	triton::TetgenFacade f;
-        f.generateTetMesh(boundary_mesh, vol_mesh);
+        f.generateTetMesh(plc, vol_mesh);
 
         return 0;
 }
