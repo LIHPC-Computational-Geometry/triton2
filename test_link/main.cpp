388991eb44f85ef30ff0d194a6e1c11a5f948b3b
Benoit LELANDAIS <benoit.lelandais@cea.fr>
2023-01-16 15:51:07 +0100

    Wrong include path

--- test_link/main.cpp
+++ test_link/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 /*----------------------------------------------------------------------------*/
-#include <Triton2/TetgenInterface/TetgenFacade.h>
+#include <Triton2/TetgenFacade.h>
 /*----------------------------------------------------------------------------*/
 using namespace gmds;
 int main() {
